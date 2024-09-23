0f56b8d29b35c3d68e6374891c9625a2fce4a9ba
DaviSnart <davi.soares9876543210@gmail.com>
2024-09-23 15:24:09 -0300

    alterações básicas

--- server.js
+++ server.js
@@ -1,73 +1,60 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
-import express from "express"
+const app = express();
+const prisma = new PrismaClient();
+const port = process.env.PORT || 3000;
 
-import { PrismaClient } from '@prisma/client'
-
+app.use(express.json());
 app.use(
-    cors({
-      origin: 'https://cadastro-de-usuarios-2-0.netlify.app/',
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
-  
-const prisma = new PrismaClient()
+  cors({
+    origin: "https://cadastro-de-usuarios-2-0.netlify.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-const port = process.env.PORT || 3000
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-app.get('/usuarios', async (request, response) => {
-
-    const users = await prisma.user.findMany()
-
-    response.status(200).json(users)
-
+app.get("/usuarios", async (request, response) => {
+  const users = await prisma.user.findMany();
+  response.status(200).json(users);
 });
 
-app.post('/usuarios', async (request, response) => {
+app.post("/usuarios", async (request, response) => {
+  await prisma.user.create({
+    data: {
+      email: request.body.email,
+      age: request.body.age,
+      name: request.body.name,
+    },
+  });
+  response.status(201).json({ Message: "Usuário cadastrado com sucesso" });
+});
 
-    await prisma.user.create({
-        data: {
-            email: request.body.email,
-            age: request.body.age,
-            name: request.body.name
-        }
-    })
+app.put("/usuarios/:id", async (request, response) => {
+  const user = await prisma.user.update({
+    where: {
+      id: request.params.id,
+    },
+    data: {
+      email: request.body.email,
+      age: request.body.age,
+      name: request.body.name,
+    },
+  });
+  response.status(200).json(user);
+});
 
-    response.status(201).json({ Message: "Usuário cadastrado com sucesso" })
+app.delete("/usuarios/:id", async (request, response) => {
+  await prisma.user.delete({
+    where: {
+      id: request.params.id,
+    },
+  });
+  response.status(200).json({ Message: "Êeeeeeee, meu parceiro! Tu apagou o cara mesmo né. Mó paia issuaí" });
+});
 
-})
-
-app.put('/usuarios/:id', async (request, response) => {
-
-    const user = await prisma.user.update({
-        where: {
-            id: request.params.id
-        },
-        data: {
-            email: request.body.email,
-            age: request.body.age,
-            name: request.body.name
-        }
-    })
-
-    response.status(200).json(user)
-
-})
-
-app.delete('/usuarios/:id', async (request, response) =>{
-    await prisma.user.delete({
-        where: {
-            id: request.params.id
-        }
-    })
-    response.status(200).json({Message: "Êeeeeeee, meu parceiro! Tu apagou o cara mesmo né. Mó paia issuaí"})
-})
-
-app.listen(port)
+app.listen(port);
 
 /* 
 
